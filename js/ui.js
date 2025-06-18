9fb16705ed4897c990ba3ce05c481ed55ee1ba57
TrevorBrowning <trevryanbrowning@gmail.com>
2025-06-18 04:21:22 -0400

    feat: Implement 'Add task to Firestore' functionality

--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,7 @@
 
 // Import Functions
 import { signInWithGitHub, logout } from "./auth.js";
+import { addTask } from "./firestore.js";
 
 // Setup Button Event Listeners (Placeholder - backend-test.html)
 
@@ -14,6 +15,18 @@ export function setupUIEventListeners() {
   const testLogoutBtn = document.getElementById("logout-button");
   if (testLogoutBtn) {
     testLogoutBtn.addEventListener("click", logout);
+  }
+
+  // Event Listner for form submit button - saves task
+  const saveTask = document.getElementById("submit-data");
+  if (saveTask) {
+    saveTask.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const titleInput = document.getElementById("title-input").value;
+      const descInput = document.getElementById("desc-input").value;
+      addTask(titleInput, descInput);
+      saveTask.reset();
+    });
   }
 }
 
