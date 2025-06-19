4d09465acf4621fb41df505bc715f6340a5b1a58
Trevor Browning <102438531+TrevorBrowning@users.noreply.github.com>
2025-06-19 01:25:50 -0400

    Revert "feat/implemented a delete task feature (#35)"
    This reverts commit 01767d81cc7faf82d21550de8c6f71589cfbb68c.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@
 // Import Functions
 import { setupAuthListeners } from "./auth.js";
 import { setupUIEventListeners } from "./ui.js";
-import { setupFirestoreListeners } from "./firestore.js";
 
 // Activate Functions
 setupAuthListeners();
 setupUIEventListeners();
-setupFirestoreListeners();
 
 console.log("App has been started from main.js");
